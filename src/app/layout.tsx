fd3bc41fc99350cac35612360f128a4223671bf0
Patil-Nandini <156404898+Patil-Nandini@users.noreply.github.com>
2025-09-24 20:20:42 +0530

    changezs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
+
 import { Sarabun } from "next/font/google";
 import "./globals.css"; 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
 
 const sarabun = Sarabun({
   subsets: ["latin"],
@@ -16,13 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }: { readonly children: React.ReactNode }) {
   return (
     <html lang="en" className={sarabun.className}>
-      <body className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-1">
-          {children}
-        </main>
-        <Footer />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
